df53f8c6e68949807c65ee56ad589bb1a45f2d31
Irfan Daimori <daimori1995@gmail.com>
2021-12-05 12:37:30 +0700

    menambah file belajar git

---
+++ pertemuan-1.py
@@ -0,0 +1,48 @@
+# ini comment
+
+# ini nama
+print("Halo saya Adit")
+# ini alamat saya
+print('Rumah saya di Solo')
+
+x = 5 # Integer
+y = "Halo, dunia" # String
+z = 2.5 # Float
+a = True # Boolean
+
+print(a)
+print(x)
+print(y)
+print(z)
+
+# Type Data
+print(type(a))
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Math operation
+p = 2
+q = 3
+r = p + q
+
+print(p ** q)
+print(r)
+
+# Casting
+x = int(2.5)
+y = str(-3.7)
+z = float(y)
+
+print(x)
+print(y)
+print(z)
+
+print(x + z)
+
+x = 'y'
+x = 5
+y = x
+z = y
+
+print(x)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+x = 'y'
+x = 5
+y = x
+z = y
+
+print(x)
